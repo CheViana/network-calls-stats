c8dee16e3b3515bfeb82eb427519419c2e7fb053
cheviana <not-so-fast@example.com>
2020-12-04 12:43:17 -0500

    example 2 more stats

--- example-2-aiohttp-send-more-stats.py
+++ example-2-aiohttp-send-more-stats.py
@@ -106,17 +106,74 @@ def profile(f=None, metric_name=None):
 # https://docs.aiohttp.org/en/stable/tracing_reference.html
 
 
+def compute_elapsed_and_send_stats(start_time, metric_name, params):
+    elapsed_time = round((
+        asyncio.get_event_loop().time() - start_time
+    ) * 1000)
+    tags = {}
+    if hasattr(params, 'host'):
+        tags['domain'] = params.host
+    if hasattr(params, 'url'):
+        tags['domain'] = params.url.raw_host
+    send_stats(
+        metric_name,
+        elapsed_time,
+        tags
+    )
+
+
 async def on_request_start(session, trace_config_ctx, params):
     trace_config_ctx.request_start = asyncio.get_event_loop().time()
 
 
 async def on_request_end(session, trace_config_ctx, params):
-    elapsed_time = round((
-        asyncio.get_event_loop().time() - trace_config_ctx.request_start
-    ) * 1000)
-    send_stats(
+    compute_elapsed_and_send_stats(
+        trace_config_ctx.request_start,
         'aiohttp_request_exec_time',
-        elapsed_time,
+        params
+    )
+
+
+async def on_connection_queued_start(session, trace_config_ctx, params):
+    trace_config_ctx.connection_queued_start = asyncio.get_event_loop().time()
+
+
+async def on_connection_queued_end(session, trace_config_ctx, params):
+    compute_elapsed_and_send_stats(
+        trace_config_ctx.connection_queued_start,
+        'aiohttp_connection_queued_time',
+        params
+    )
+
+
+async def on_connection_create_start(session, trace_config_ctx, params):
+    trace_config_ctx.connection_create_start = asyncio.get_event_loop().time()
+
+
+async def on_connection_create_end(session, trace_config_ctx, params):
+    compute_elapsed_and_send_stats(
+        trace_config_ctx.connection_create_start,
+        'aiohttp_connection_create_time',
+        params
+    )
+
+
+async def on_dns_resolvehost_start(session, trace_config_ctx, params):
+    trace_config_ctx.dns_resolvehost_start = asyncio.get_event_loop().time()
+
+
+async def on_dns_resolvehost_end(session, trace_config_ctx, params):
+    compute_elapsed_and_send_stats(
+        trace_config_ctx.dns_resolvehost_start,
+        'aiohttp_dns_resolvehost_time',
+        params
+    )
+
+
+async def on_request_redirect(session, trace_config_ctx, params):
+    send_stats(
+        'aiohttp_request_redirect',
+        1,
         {'domain': params.url.raw_host}
     )
 
@@ -125,7 +182,41 @@ async def on_request_exception(session, trace_config_ctx, params):
     send_stats(
         'aiohttp_request_exception',
         1,
-        {'domain': params.url.raw_host, 'exception_class': params.exception.__class__.__name__}
+        {
+            'domain': params.url.raw_host,
+            'exception_class': params.exception.__class__.__name__
+        }
+    )
+
+
+async def on_response_chunk_received(session, trace_config_ctx, params):
+    send_stats(
+        'aiohttp_response_chunk_received',
+        len(params.chunk),
+        {'domain': params.url.raw_host}
+    )
+
+
+async def on_connection_reuseconn(session, trace_config_ctx, params):
+    send_stats(
+        'aiohttp_connection_reuseconn',
+        1
+    )
+
+
+async def on_dns_cache_hit(session, trace_config_ctx, params):
+    send_stats(
+        'aiohttp_dns_cache_hit',
+        1,
+        {'domain': params.host}
+    )
+
+
+async def on_dns_cache_miss(session, trace_config_ctx, params):
+    send_stats(
+        'aiohttp_dns_cache_miss',
+        1,
+        {'domain': params.host}
     )
 
 
@@ -134,7 +225,18 @@ class Profiler(TraceConfig):
         super().__init__(*args, **kwargs)
         self.on_request_start.append(on_request_start)
         self.on_request_end.append(on_request_end)
+        self.on_request_redirect.append(on_request_redirect)
         self.on_request_exception.append(on_request_exception)
+        self.on_connection_queued_start.append(on_connection_queued_start)
+        self.on_connection_queued_end.append(on_connection_queued_end)
+        self.on_connection_create_start.append(on_connection_create_start)
+        self.on_connection_create_end.append(on_connection_create_end)
+        self.on_dns_resolvehost_start.append(on_dns_resolvehost_start)
+        self.on_dns_resolvehost_end.append(on_dns_resolvehost_end)
+        self.on_response_chunk_received.append(on_response_chunk_received)
+        self.on_connection_reuseconn.append(on_connection_reuseconn)
+        self.on_dns_cache_hit.append(on_dns_cache_hit)
+        self.on_dns_cache_miss.append(on_dns_cache_miss)
 
 
 # ----------------------------- execute async backend requests -----------------------------------
